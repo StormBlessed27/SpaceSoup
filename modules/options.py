4689016dd1edfdbbc0ea6e834a3d0a71ac3b9882
Hemerson Ballen <hjballent@gmail.com>
2023-04-16 17:25:52 -0500

    Adding the route for Big asteroids sprite

--- modules/options.py
+++ modules/options.py
@@ -1,7 +1,8 @@
 WIDTH = 1000
 HEIGHT = 600
 SPEED = 3
-ASTEROID_IMG = "assets/sprites/meteorGrey_med1.png"
+MED_ASTEROID_IMG = "assets/sprites/meteorGrey_med1.png"
+BIG_ASTEROID_IMG = "assets/sprites/meteorGrey_big2.png"
 SHIP_IMG= "assets/sprites/ship-2.png"
 BACKGROUND_IMG = "assets/images/fondo4.png"
 BLACK = (0,0,0)

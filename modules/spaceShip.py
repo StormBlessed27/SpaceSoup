b483c55e3a91ed62e426ece56cd143f260befb8d
Hemerson Ballen <hjballent@gmail.com>
2023-04-16 16:42:18 -0500

    spaceShip class complete with move

--- modules/spaceShip.py
+++ modules/spaceShip.py
@@ -3,39 +3,31 @@ from modules import options
 
 class spaceShip(pygame.sprite.Sprite):
     '''Create the player SpaceShip'''
-
     def __init__(self):
-       super().__init__()
-       self.image = pygame.image.load("assets/images/ship-2.png")
-       self.image.set_colorkey("BLACK")
-       self.rect=self.image.get_rect()
-       self.rect.centerx= options.WINDOW_WIDTH//2
-       self.rect.bottom=options.WINDOW_HEIGHT-10
-       self.shipSpeed=4
-       
+      super().__init__()
+      self.image = pygame.image.load(options.SHIP_IMG).convert()
+      self.image.set_colorkey(options.BLACK)
+      self.rect = self.image.get_rect()
+      self.rect.centerx= options.WIDTH//2 
+      self.rect.centery= options.HEIGHT//2
+      self.speed = options.SPEED
 
     def Move(self):
-        teclas_pres = pygame.key.get_pressed()
+      """Move the spaceship"""
+      keysState = pygame.key.get_pressed()
+      if keysState[pygame.K_d] or keysState[pygame.K_RIGHT]:
+        self.rect.x += self.speed 
+      elif keysState[pygame.K_a] or keysState[pygame.K_LEFT]:
+        self.rect.x -= self.speed
 
-        if teclas_pres[pygame.K_d] or teclas_pres[pygame.K_RIGHT]:
-          self.rect.x += self.shipSpeed 
-        elif teclas_pres[pygame.K_a] or teclas_pres[pygame.K_LEFT]:
-          self.rect.x -= self.shipSpeed
+      if keysState[pygame.K_s] or keysState[pygame.K_DOWN]:
+        self.rect.y += self.speed
+      elif keysState[pygame.K_w] or keysState[pygame.K_UP]:
+        self.rect.y -= self.speed
 
-        if teclas_pres[pygame.K_s] or teclas_pres[pygame.K_DOWN]:
-          self.rect.y += self.shipSpeed
-        elif teclas_pres[pygame.K_w] or teclas_pres[pygame.K_UP]:
-          self.rect.y -= self.shipSpeed
-
-    def Shoot():
-        algotambien = 0
-
- 
-    def Draw(self, Surface: pygame.Surface):
-       shipSurface = pygame.image.load("SpaceSoup-main/assets/images/ship-2.png")
-       Surface.blit(shipSurface, self.rect)
-
-
-    
-    def Hit(self):
-      self.health -=1
+    def Limit(self):
+      """Adds colitions with the window limits"""
+      if self.rect.top <=0: self.rect.top=0
+      if self.rect.bottom >= options.HEIGHT: self.rect.bottom = options.HEIGHT
+      if self.rect.left <=0: self.rect.left=0
+      if self.rect.right >= options.WIDTH: self.rect.right=options.WIDTH

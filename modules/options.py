db1100e912d3cd0bb402657401609309f6f2e17d
Hemerson Ballen <hjballent@gmail.com>
2023-04-16 16:41:48 -0500

    Adding new options

--- modules/options.py
+++ modules/options.py
@@ -1,3 +1,7 @@
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
-SHIPSPEED = 3
+WIDTH = 1000
+HEIGHT = 600
+SPEED = 3
+ASTEROID_IMG = "assets/sprites/meteorGrey_med1.png"
+SHIP_IMG= "assets/sprites/ship-2.png"
+BACKGROUND_IMG = "assets/images/fondo4.png"
+BLACK = (0,0,0)

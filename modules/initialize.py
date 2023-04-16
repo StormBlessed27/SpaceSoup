96833d9c0f484eb970fc01b307076367ab42431d
Hemerson Ballen <hjballent@gmail.com>
2023-04-16 17:25:15 -0500

    Adding greaters asteroids and Speed Increase event

--- modules/initialize.py
+++ modules/initialize.py
@@ -6,8 +6,12 @@ from modules import SpaceShip, options, enemies
 def initialize():
   
   #Initialize screen and pygame motor
+
+  
   pygame.init()
   pygame.mixer.init()
+  EVENT_INCREASE_SPEED = pygame.USEREVENT+1
+  pygame.time.set_timer(EVENT_INCREASE_SPEED, 30000,7)
   screen = pygame.display.set_mode((options.WIDTH, options.HEIGHT))
   background = pygame.image.load(options.BACKGROUND_IMG)
   pygame.display.set_caption("Space Soup")
@@ -21,9 +25,13 @@ def initialize():
   sprites.add(ship)
 
   for i in range(9):
-    asteroid = enemies.Asteroid(options.ASTEROID_IMG)
+    if i%2 ==0:
+      asteroid = enemies.Asteroid(options.MED_ASTEROID_IMG)
+    else:
+      asteroid = enemies.Asteroid(options.BIG_ASTEROID_IMG)
     sprites.add(asteroid)
     asteroids.add(asteroid)
+
 
 
   while is_running:
@@ -31,24 +39,23 @@ def initialize():
       if event.type == pygame.QUIT:
         pygame.quit()
         is_running = False
+      if event.type == EVENT_INCREASE_SPEED:
+        for asteroid in asteroids:
+          asteroid.IncreaseSpeed()
 
     screen.blit(background,(0,0))
     ship.Move()
     ship.Limit()
     for asteroid in asteroids:
       asteroid.Move()
+      print(asteroid.speedy)
+
+
     sprites.update()
     sprites.draw(screen)
 
     pygame.display.flip()
 
-    # if ship.rect.top <=0 or ship.rect.bottom >= options.WINDOW_HEIGHT:
-    #   if ship.rect.top <= 0: ship.rect.top=0
-    #   if ship.rect.bottom >= options.WINDOW_HEIGHT: ship.rect.bottom = options.WINDOW_HEIGHT
-    # if ship.rect.left <=0 or ship.rect.right >= options.WINDOW_WIDTH:
-    #   if ship.rect.left <= 0: ship.rect.left=0
-    #   if ship.rect.right >= options.WINDOW_WIDTH: ship.rect.right=options.WINDOW_WIDT
-    
     gameClock.tick(60)
 
 
